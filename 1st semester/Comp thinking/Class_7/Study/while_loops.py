11788782f05ffe03ec134053609c49df16c1311a
ValiTheValista <ernestocoolrocks@gmail.com>
2024-08-27 14:25:07 -0600

    Created a few examples of while loops

---
+++ 1st semester/Comp thinking/Class_7/Study/while_loops.py
@@ -0,0 +1,42 @@
+""" Add # before this to enable code
+
+#Prints a list of numbers from 1 to 100
+x = 0
+while x < 100:
+    x += 1
+    print(x)
+    
+#"""
+
+""" Add # before this to enable code
+
+#Prints the fibonacci sequence up to n
+# Fn = Fn-1 + Fn-2   
+a, b, n = 0, 1, 4181
+
+
+while b <= n:   
+    print(b) 
+    a,b = b, a+b
+    
+
+
+""" 
+
+""" Add # before this to enable code
+# This while looking the same produces a different result
+ 
+a = 0
+b = 1
+n = 4181
+
+while b <= 4181:
+    print(b)
+    b = a+b
+    a = b
+
+    
+    
+#"""
+
+
