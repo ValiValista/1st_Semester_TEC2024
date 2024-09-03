bd3b844fe4429d7e860b82f4bf49ecb2f54bdd14
ValiValista <a01647566@tec.mx>
2024-09-03 10:23:24 -0600

    Updated localization files.

---
+++ 1st semester/Comp thinking/Tests/test1.py
@@ -0,0 +1,10 @@
+def funcion_uno(a, b) :
+    a = a + 3
+    c = b / 2
+    return a + c
+def main() :
+    valor1 = 5
+    valor2 = 10
+    respuesta = funcion_uno(valor1, valor2)
+    print(respuesta)
+main()
