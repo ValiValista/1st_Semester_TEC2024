f3bdb74335995025b73d14422f79841e01265bad
ValiValista <a01647566@tec.mx>
2024-09-03 10:24:03 -0600

    Created the midterm practical exam file
    Updated localization files.

---
+++ 1st semester/Comp thinking/Class_8/Exams/Exam_a01647566.py
@@ -0,0 +1,21 @@
+"""
+Problem guideline:
+
+Inputs:
+
+Process Variables:
+
+Process Constants:
+
+Outputs:
+
+
+Dont forget!
+    Check if greater than 0
+    Convert to int, float, str, in their respective circumstances
+    Put everything into functions and call them in main
+
+        If menus are used: used While True: and sys.exit()
+
+
+"""
